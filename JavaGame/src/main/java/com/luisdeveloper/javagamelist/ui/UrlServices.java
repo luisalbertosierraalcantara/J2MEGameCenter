53772a8f8f44ed69a8ca640714f57e70c0c6d7ac
Ing. Luis A. Sierra <33613937+luisalbertosierraalcantara@users.noreply.github.com>
2019-01-13 02:17:15 -0400

    Add files via upload

--- JavaGame/src/main/java/com/luisdeveloper/javagamelist/ui/UrlServices.java
+++ JavaGame/src/main/java/com/luisdeveloper/javagamelist/ui/UrlServices.java
@@ -2,5 +2,5 @@ package com.luisdeveloper.javagamelist.ui;
 
 public class UrlServices {
     public static final String RESTSERVICE =
-            "https://raw.githubusercontent.com/luisalbertosierraalcantara/J2MEGameCenter/master/REST/ListaPrincipal.json";
+            "https://raw.githubusercontent.com/luisalbertosierraalcantara/J2MEGameCenter/master/JarDemo/ListaPrincipal.json";
 }
